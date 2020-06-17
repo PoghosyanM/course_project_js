fbf286e2f494b0fb797014dcc6933fbc2f8dc873
Mihran <poghosyan.mihran.96@gmail.com>
2020-06-17 19:54:49 +0400

    move searchs functionality out of app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { getUsers } from "./api/api.js";
-import { createUserInfoElem } from "./utils/helpers.js";
+import { createUserInfoElem, getFilteredUsers } from "./utils/helpers.js";
 const rootElem = document.getElementById("root");
 const usersContainer = document.createElement("div");
 const header = document.createElement("header");
@@ -16,22 +16,7 @@ usersContainer.setAttribute("id", "usersContainer");
 async function users() {
   try {
     const data = await getUsers(10);
-    searchInput.addEventListener("input", (e) => {
-      usersContainer.innerHTML = "";
-      const inputValue = e.currentTarget.value;
-      const filteredUsers = data.results.filter((user) => {
-        const {
-          name: { first, last },
-        } = user;
-        return (
-          first.toUpperCase().includes(inputValue.toUpperCase()) ||
-          last.toUpperCase().includes(inputValue.toUpperCase())
-        );
-      });
-      filteredUsers.forEach(function (user) {
-        createUserInfoElem(user, usersContainer);
-      });
-    });
+    getFilteredUsers(data, usersContainer, searchInput);
     data.results.forEach(function (user) {
       createUserInfoElem(user, usersContainer);
     });
