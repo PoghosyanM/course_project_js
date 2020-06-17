00896c98ab5f499692e9b5fe80aee6799c816812
Vah007 <tshughurian@gmail.com>
2020-06-17 22:38:47 +0400

    tnayin

--- app.js
+++ app.js
@@ -1,39 +1,30 @@
 import { getUsers } from "./api/api.js";
-import { createUserInfoElem } from "./utils/helpers.js";
+import { createUserInfoElem, userSearch } from "./utils/helpers.js";
 const rootElem = document.getElementById("root");
+console.log(rootElem);
 const usersContainer = document.createElement("div");
 const header = document.createElement("header");
 const searchInput = document.createElement("input");
 
 header.setAttribute("id", "header");
 searchInput.setAttribute("id", "searchInput");
-
+header.innerHTML = `Search User `
 header.append(searchInput);
 rootElem.append(header);
 
 usersContainer.setAttribute("id", "usersContainer");
 
+
 async function users() {
   try {
     const data = await getUsers(10);
-    searchInput.addEventListener("input", (e) => {
-      usersContainer.innerHTML = "";
-      const inputValue = e.currentTarget.value;
-      const filteredUsers = data.results.filter((user) => {
-        const {
-          name: { first, last },
-        } = user;
-        return (
-          first.toUpperCase().includes(inputValue.toUpperCase()) ||
-          last.toUpperCase().includes(inputValue.toUpperCase())
-        );
-      });
-      filteredUsers.forEach(function (user) {
-        createUserInfoElem(user, usersContainer);
-      });
-    });
+
+    userSearch(data);   
+
     data.results.forEach(function (user) {
       createUserInfoElem(user, usersContainer);
+      console.log(user);
+      
     });
   } catch (error) {
     console.log(error);
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,13 +1,46 @@
 export function createUserInfoElem(userData, mainContent) {
-  const { name, picture } = userData;
+  const { name, picture, gender, nat, email, dob, location, login, phone } = userData;
 
   const content = document.createElement("div");
   const image = document.createElement("img");
-  const fullname = document.createElement("h2");
+  const fullName = document.createElement("h3");
+  fullName.setAttribute("id", "id_h3")
+  const infoUser = document.createElement("h4");
+  infoUser.setAttribute("id", "id_h4")
+
 
   image.setAttribute("src", picture.large);
-  fullname.innerText = `${name.first} ${name.last}`;
-  content.append(fullname, image);
-  content.setAttribute("id", "userData");
+  fullName.innerHTML = `<table> <li> Name Surname - ${name.first} ${name.last} </li>
+                                <li> Age - ${dob.age} </li>
+                                <li> Country , City - ${location.country} , ${location.city} </li>
+                                <li> Adress - ${location.street.name} </li>
+                                <li> Phone - ${phone} </li> </table>`
+
+  infoUser.innerHTML = `<table> <li> E-Mail - ${email} </li>
+                                <li> National - ${nat} </li>
+                                <li> Gender - ${gender} </li>
+                                <li> Login - ${login.username} </li> </table>`
+
+  
+  content.append(image, fullName, infoUser );
   mainContent.append(content);
+}
+
+export function userSearch(data) {
+  searchInput.addEventListener("input", (e) => {
+    usersContainer.innerHTML = "";
+    const inputValue = e.currentTarget.value;
+    const filteredUsers = data.results.filter((user) => {
+      const {
+        name: { first, last },
+      } = user;
+      return (
+        first.toUpperCase().includes(inputValue.toUpperCase()) ||
+        last.toUpperCase().includes(inputValue.toUpperCase())
+      );
+    });
+    filteredUsers.forEach(function (user) {
+      createUserInfoElem(user, usersContainer);
+    });
+  });
 }
