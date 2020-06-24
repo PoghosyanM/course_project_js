d5120924dd5004723635917ac7fc67af91e2d84b
Mihran <poghosyan.mihran.96@gmail.com>
2020-06-24 21:20:07 +0400

    add nations

--- utils/nationsData.js
+++ utils/nationsData.js
@@ -4,11 +4,35 @@ export const nations = [
     text: "Random",
   },
   {
+    value: "au",
+    text: "Australia",
+  },
+  {
+    value: "br",
+    text: "Brasil",
+  },
+  {
+    value: "ch",
+    text: "China",
+  },
+  {
     value: "de",
     text: "Germany",
   },
   {
     value: "fr",
     text: "France",
+  },
+  {
+    value: "gb",
+    text: "Great Britain",
+  },
+  {
+    value: "nl",
+    text: "Nederlands",
+  },
+  {
+    value: "us",
+    text: "United States",
   },
 ];
